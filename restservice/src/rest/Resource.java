a48433dddaf3a8cb70c992f012dcaaf1ef2f79b5
palse <palse@local>
2017-04-24 01:27:39 +0900

    test

---
+++ restservice/src/rest/Resource.java
@@ -0,0 +1,112 @@
+
+package rest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/rest")
+@MultipartConfig()
+public class Resource {
+	
+	@POST
+	@Path("/upload")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String callUpload(@Context HttpServletRequest request){
+		String json = null;
+		
+		System.out.println("call upload");
+		
+		try {
+			for (Part part : request.getParts()) {
+			    String fileName = "upload.zip";
+			    System.out.println(fileName);
+			    
+			    //part.write(uploadFilePath + File.separator + fileName);
+			    part.write(fileName);
+			}
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (ServletException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		
+//		try {
+//			Part part = request.getPart("upload");
+//			System.out.println(part.getSubmittedFileName());
+// 			System.out.println(part.getSize());
+//			part.write("/Users/hiro62521/Desktop/upload.zip");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (ServletException e) {
+//			e.printStackTrace();
+//		}
+		
+		json = "{\"result\":\"SUCCESS\"}";
+		
+		return json;
+		
+	}
+	
+	@GET
+	@Path("/download")
+	public Response callDownload(@Context HttpServletResponse response){
+		
+		InputStream in = null;
+		OutputStream out = null;
+		
+		File file = new File("/Users/hiro62521/Desktop/test.zip");
+		
+		response.setContentType("application/zip");
+		response.setHeader("Content-Disposition", "attachment; filename=download.zip"); 
+		
+		try {
+			in = new FileInputStream(file);
+			out = response.getOutputStream();
+			
+			byte[] b = new byte[1024];
+			int len = 0;
+			while((len = in.read(b)) != -1){
+				out.write(b);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(in != null){
+				try {
+					in.close();
+				} catch (IOException e) {
+				}
+			}
+			if(out != null){
+				try {
+					out.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		
+		return Response.ok().build();
+	}
+
+}
